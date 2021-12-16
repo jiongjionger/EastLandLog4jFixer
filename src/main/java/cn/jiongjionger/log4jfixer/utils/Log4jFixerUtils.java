043e82a02a4e2fc455d8d4252ac1c95b62f241e9
jiongjionger <17jiong@gmail.com>
2021-12-16 22:18:57 +0800

    修复部分版本log4j2成员变量“lookups”为“strLookupMap”的问题

--- src/main/java/cn/jiongjionger/log4jfixer/utils/Log4jFixerUtils.java
+++ src/main/java/cn/jiongjionger/log4jfixer/utils/Log4jFixerUtils.java
@@ -12,6 +12,7 @@ import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.apache.logging.log4j.core.lookup.Interpolator;
 import org.apache.logging.log4j.core.lookup.StrLookup;
+import org.bukkit.Bukkit;
 
 import javax.annotation.Nullable;
 
@@ -28,12 +29,30 @@ public class Log4jFixerUtils {
      */
     private static void initInterpolatorField() {
         try {
-            lookupsField = Interpolator.class.getDeclaredField("lookups");
-            lookupsField.setAccessible(true);
+            lookupsField = getLookupsField();
+            if (lookupsField != null) {
+                lookupsField.setAccessible(true);
+            }
             configField = getConfigField();
-            configField.setAccessible(true);
+            if (configField != null) {
+                configField.setAccessible(true);
+            }
         } catch (Exception ignore) {
         }
+    }
+
+    /**
+     * 根据不同log4j版本获取对应的lookups
+     *
+     * @return 获取的lookups，如果获取失败则返回null
+     */
+    @Nullable
+    private static Field getLookupsField() {
+        Field field = getDeclaredFieldWithSuperClass(Interpolator.class, "lookups");
+        if (field == null) {
+            field = getDeclaredFieldWithSuperClass(Interpolator.class, "strLookupMap");
+        }
+        return field;
     }
 
     /**
@@ -91,21 +110,29 @@ public class Log4jFixerUtils {
      * @return 修复成功则返回true，否则返回false
      */
     public static boolean tryFix() {
-        if (lookupsField == null || configField == null) {
+        if (lookupsField == null) {
+            Bukkit.getLogger().warning("[EastLandLog4jFixer] LookupsField is null!");
+            return false;
+        }
+        if (configField == null) {
+            Bukkit.getLogger().warning("[EastLandLog4jFixer] ConfigField is null!");
             return false;
         }
         boolean status = false;
         try {
-            final org.apache.logging.log4j.core.Logger rootLogger = (org.apache.logging.log4j.core.Logger) LogManager.getRootLogger();
-            final StrLookup strLookup = rootLogger.getContext().getConfiguration().getStrSubstitutor().getVariableResolver();
+            final org.apache.logging.log4j.core.Logger rootLogger = (org.apache.logging.log4j.core.Logger)
+                    LogManager.getRootLogger();
+            final StrLookup strLookup = rootLogger
+                    .getContext()
+                    .getConfiguration()
+                    .getStrSubstitutor()
+                    .getVariableResolver();
             if (strLookup instanceof Interpolator) {
                 clearLookups((Interpolator) strLookup);
             }
             final Map<String, Appender> appenders = rootLogger.getAppenders();
             if (!appenders.isEmpty()) {
-                Iterator<Map.Entry<String, Appender>> iterator = appenders.entrySet().iterator();
-                while (iterator.hasNext()) {
-                    Map.Entry<String, Appender> entry = iterator.next();
+                for (Map.Entry<String, Appender> entry : appenders.entrySet()) {
                     final Appender appender = entry.getValue();
                     final Layout<? extends Serializable> layout = appender.getLayout();
                     if (!(layout instanceof PatternLayout)) {
@@ -129,15 +156,15 @@ public class Log4jFixerUtils {
      * 清空指定Interpolator类中的lookups（Map）变量
      */
     private static void clearLookups(Interpolator interpolator) {
-        if (lookupsField == null) {
-            return;
-        }
         try {
+            Bukkit.getLogger().info("[EastLandLog4jFixer] Try to clear lookups..");
             Object lookupsObject = lookupsField.get(interpolator);
             if (lookupsObject instanceof Map) {
                 ((Map<?, ?>) lookupsObject).clear();
             }
+            Bukkit.getLogger().info("[EastLandLog4jFixer] Successfully clear lookups!");
         } catch (Exception ignore) {
+            Bukkit.getLogger().warning("[EastLandLog4jFixer] Try to clear lookups failed!");
         }
     }
 }
